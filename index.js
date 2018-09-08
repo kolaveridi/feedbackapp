21b59c2453324cd360d3cffa36297a004acca180
satya_jha <satyajeetjha06@gmail.com>
2018-09-08 19:53:22 +0530

    googl oauth done

--- index.js
+++ index.js
@@ -3,11 +3,30 @@ const express=require('express');
 const app=express();
 const passport =require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys=require('./config/keys');
+console.log('keys',keys);
+let googleClientID=keys.googleClientID;
+let googleClientSecret=keys.googleClientSecret;
 
-
-//clientId 
-//clientSecret 
-passport.use(new GoogleStrategy());
+// //clientId 
+// //clientSecret 
+passport.use(new GoogleStrategy({
+    clientID:googleClientID,
+    clientSecret:googleClientSecret,
+    callbackURL: '/auth/google/callback'
+},
+(accessToken,refreshToken,profile,done)=>{
+  console.log('a1',accessToken);
+  console.log('a2',refreshToken);
+  console.log('a3',profile);
+  console.log('a4',done);
+})
+);
+app.get('/auth/google',
+   passport.authenticate('google',{
+    scope:['profile','email']
+}));
+app.get('/auth/google/callback',passport.authenticate('google'));
 
 const PORT=process.env.PORT || 5000;
 
