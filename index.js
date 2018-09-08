8fb5e1adb50c9bba14b5eccba06a304f5d23fdb4
satya_jha <satyajeetjha06@gmail.com>
2018-09-08 18:42:40 +0530

    simple chnages

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 const express=require('express');
 const app=express();
+const passport =require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 
-app.get('/',(req,res)=>{
-  res.send({hi:'there'});
-});
-
+//clientId 
+//clientSecret 
+passport.use(new GoogleStrategy());
 
 const PORT=process.env.PORT || 5000;
 
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
