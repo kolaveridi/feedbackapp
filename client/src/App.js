262eb4da80feeda0d23cd23fe6fd37c35f69c0a8
satya_jha <satyajeetjha06@gmail.com>
2018-09-12 19:24:12 +0530

    added proxy thing in package.json

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <h1>Hello react </h1>
+         <a href="http://localhost:5000/auth/google">Sign in with Google </a>
+         </header>
       </div>
     );
   }
