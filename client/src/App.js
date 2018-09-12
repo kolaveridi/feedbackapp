4f124ea7cec2fa7385990db1581fefa09800fb16
satya_jha <satyajeetjha06@gmail.com>
2018-09-12 19:44:14 +0530

    proxy server to be created in react package.json instead of main so fixed this

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
          <h1>Hello react </h1>
-         <a href="http://localhost:5000/auth/google">Sign in with Google </a>
+         <a href="/auth/google">Sign in with Google </a>
          </header>
       </div>
     );
