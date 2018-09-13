3cbfe671391b21deb2650a663d06e79528b4cf80
satya_jha <satyajeetjha06@gmail.com>
2018-09-13 22:43:35 +0530

    react-router-routing part

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter,Route} from 'react-router-dom';
+const Header =()=><h1>Header</h1>;
+const Dashbaord =()=><h1>Dashbaord</h1>;
+const SurveyNew =()=><h1>SurveyNew</h1>;
+const Landing =()=><h1>Landing</h1>;
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-         <h1>Hello react </h1>
-         <a href="/auth/google">Sign in with Google </a>
-         </header>
+        <BrowserRouter>
+        <div>
+        
+          <Route path="/surveys" component={Dashbaord}/>
+          <Route  exact={true} path="/" component={Landing}/>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
