7e215257a88384d9bf6fd9b1070e6d59a33f5dc7
satya_jha <satyajeetjha06@gmail.com>
2018-09-23 21:08:48 +0530

    subdoc collectios and route thing

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,11 +1,25 @@
 
+const mongoose=require('mongoose');
 const requireLogin=require('../middlewares/requireLogin');
 const requireCredits=require('../middlewares/requireCredits');
 // first login check and then requirecredit check
+const Survey=mongoose.model('surveys');
 module.exports= app =>{
 
   app.post('/api/surveys',requireLogin,requireCredits,(req,res,next)=>{
+    const{title,subject,body,recipients}=req.body;
+    
 
+    const survey=new Survey({
+    title,
+    subject,
+    body,
+    recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+    _user: req.user.id,
+    dateSent: Date.now()
+    });
+
+   
   })
 
 };
